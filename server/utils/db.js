8b58eb3a43cf5c27e8406d2a5dd334709b1e149a
mahansh16 <mahansh@lilagames.com>
2025-07-12 16:49:05 +0530

    Add: User Controller

---
+++ server/utils/db.js
@@ -0,0 +1,12 @@
+//X7V4ROILSGYjyJM6
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('mongodb connected successfully.');
+    } catch (error) {
+        console.log(error);
+    }
+}
+export default connectDB;
